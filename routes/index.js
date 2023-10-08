419831d366db470ed7e3643d79497ab57d6ec4b8
Josh Skouson <Jmatt6836@gmail.com>
2023-10-07 18:01:10 -0600

    lesson 4 project

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 // GET /feed/posts
+router.use('/', require('./swagger'));
 router.use('/contacts', require('./contacts'));
 // localhost:8080/professional/
 module.exports = router;
